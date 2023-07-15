bcb3c8f0396f1b6ad3e41a598f6852a72367cbda
matt <mail@mattkbrat.com>
2023-07-15 14:57:38 -0600

    @astro/image not compatible with cloudflare.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import {defineConfig} from 'astro/config';
 
 // https://astro.build/config
-import image from "@astrojs/image";
 import tailwind from '@astrojs/tailwind';
 import preact from '@astrojs/preact';
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [image(), tailwind({
+    integrations: [tailwind({
         // Example: Provide a custom path to a Tailwind config file
         configFile: './tailwind.config.js',
     }), preact()],
