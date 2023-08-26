9738d018b038b04b4c2daafcf47caeaa3b702ffb
Matt <mattkbrat@pm.me>
2023-08-26 13:32:02 -0600

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
-import {defineConfig} from 'astro/config';
+import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 import tailwind from '@astrojs/tailwind';
 import preact from '@astrojs/preact';
 
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind({
-        // Example: Provide a custom path to a Tailwind config file
-        configFile: './tailwind.config.js',
-    }), preact()],
+  integrations: [tailwind({
+    // Example: Provide a custom path to a Tailwind config file
+    configFile: './tailwind.config.js'
+  }), preact(), sitemap()]
 });
