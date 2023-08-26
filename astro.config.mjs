2ca64a2ac38bb41dcd35e164c894fe88ddcbe776
Matt <mattkbrat@pm.me>
2023-08-26 13:51:10 -0600

    Add site to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.fortmorgangrooming.com",
   integrations: [
     tailwind({
       // Example: Provide a custom path to a Tailwind config file
