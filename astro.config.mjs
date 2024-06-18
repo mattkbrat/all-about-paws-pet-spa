cb352dfe9cfc6375752e2ca6edbb07ceedb8080d
Matthew Bratrsovsky <realzing@pm.me>
2024-06-18 08:58:36 -0600

    chore(project): upgrade packages, remove @astro/prettier, init biome, add mdx support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,19 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
 
-// https://astro.build/config
 import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.fortmorgangrooming.com",
-  integrations: [
-    tailwind({
-      // Example: Provide a custom path to a Tailwind config file
-      configFile: "./tailwind.config.js",
-    }),
-    preact({ compat: true }),
-    sitemap(),
-  ],
+  integrations: [tailwind({
+    // Example: Provide a custom path to a Tailwind config file
+    configFile: "./tailwind.config.js"
+  }), preact({
+    compat: true
+  }), sitemap(), mdx()]
 });
