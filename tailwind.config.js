47c4eb568832ba2610ec3da98f79afb8c0881246
Matthew Bratrsovsky <realzing@pm.me>
2024-10-31 11:06:45 -0600

    feat(fonts): add oswald fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,40 +1,43 @@
+//
 export default {
-  content: ["./src/**/*.{html,js,tsx,astro}"],
-  theme: {
-    screens: {
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1280px",
-      "2xl": "1536px",
-      "3xl": "1920px",
-    },
-
-    colors: {
-      blue: "#1fb6ff",
-      purple: "#7e5bef",
-      pink: "#ff49db",
-      orange: "#ff7849",
-      green: "#13ce66",
-      yellow: "#ffc82c",
-      "gray-dark": "#273444",
-      gray: "#8492a6",
-      "gray-light": "#d3dce6",
-      white: "#fff",
-      transparent: "transparent",
-    },
-    fontFamily: {
-      sans: ["Graphik", "sans-serif"],
-      serif: ["Merriweather", "serif"],
-    },
-    extend: {
-      spacing: {
-        "8xl": "96rem",
-        "9xl": "128rem",
-      },
-      borderRadius: {
-        "4xl": "2rem",
-      },
-    },
-  },
+	content: ["./src/**/*.{html,js,tsx,astro}"],
+	theme: {
+		screens: {
+			sm: "640px",
+			md: "768px",
+			lg: "1024px",
+			xl: "1280px",
+			"2xl": "1536px",
+			"3xl": "1920px",
+		},
+		extend: {
+			fontFamily: {
+				display: ["Oswald-Light", "sans-serif"],
+				heading: ["Oswald", "sans-serif"],
+				body: ["Noto Sans Regular", "sans-serif"],
+			},
+			spacing: {
+				"8xl": "96rem",
+				"9xl": "128rem",
+			},
+			borderRadius: {
+				"4xl": "2rem",
+			},
+			colors: {
+				gray: {
+					50: "#f6f6f6",
+					100: "#e7e7e7",
+					200: "#d1d1d1",
+					300: "#b0b0b0",
+					400: "#808080",
+					500: "#6d6d6d",
+					600: "#5d5d5d",
+					700: "#4f4f4f",
+					800: "#454545",
+					900: "#3d3d3d",
+					950: "#262626",
+				},
+			},
+		},
+	},
 };
