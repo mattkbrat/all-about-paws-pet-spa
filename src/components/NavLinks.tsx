9f2129faa51cb184f8a0df7c77da379770348887
Matthew Bratrsovsky <realzing@pm.me>
2024-10-31 17:40:32 -0600

    fix: remove border from nav links on larger screens

--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -2,14 +2,14 @@ export const NavLinks = ({
 	selected,
 }: { selected: "home" | "packages" | "faq" }) => {
 	const selectClass =
-		"text-white lg:bg-gray-900 border-r-accent-900 lg:border-r-0 hover:bg-gray-900 ";
+		"text-white lg:bg-gray-900 border-r-accent-900 hover:bg-gray-900 ";
 
 	return (
 		<>
 			<a href="/" class={""}>
 				<li
 					id="home"
-					class={`py-6 px-4 lg:py-0 hover:bg-gray-400 border-r-4 border-r-current hover:text-gray-100 transition-colors h-full ${selected === "home" ? selectClass : ""}`}
+					class={`py-6 px-4 lg:py-0 hover:bg-gray-400 border-r-4 border-r-current lg:border-r-0 hover:text-gray-100 transition-colors h-full ${selected === "home" ? selectClass : ""}`}
 				>
 					Home
 				</li>
@@ -18,7 +18,7 @@ export const NavLinks = ({
 			<a href="/packages" class={""}>
 				<li
 					id="packages"
-					class={`py-6 px-4 lg:py-0 hover:bg-gray-400 border-r-4 border-r-current hover:text-gray-100 transition-colors h-full ${selected === "packages" ? selectClass : ""}`}
+					class={`py-6 px-4 lg:py-0 hover:bg-gray-400 border-r-4 border-r-current lg:border-r-0 hover:text-gray-100 transition-colors h-full ${selected === "packages" ? selectClass : ""}`}
 				>
 					Packages
 				</li>
@@ -26,7 +26,7 @@ export const NavLinks = ({
 			<a href="/faq" class={""}>
 				<li
 					id="faq"
-					class={`py-6 px-4 lg:py-0 hover:bg-gray-400 border-r-4 border-r-current hover:text-gray-100 transition-colors h-full ${selected === "faq" ? selectClass : ""}`}
+					class={`py-6 px-4 lg:py-0 hover:bg-gray-400 border-r-4 border-r-current lg:border-r-0 hover:text-gray-100 transition-colors h-full ${selected === "faq" ? selectClass : ""}`}
 				>
 					FAQ
 				</li>
