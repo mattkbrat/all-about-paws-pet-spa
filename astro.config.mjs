47a3db2dc67dfda2f563de882e386d791ab38448
Matthew Bratrsovsky <realzing@pm.me>
2025-02-15 16:23:59 -0700

    fix: formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,23 @@
-import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
-import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.fortmorgangrooming.com",
-  integrations: [
-    preact({
-      compat: true,
-    }),
-    sitemap(),
-    mdx(),
-  ],
+	site: "https://www.fortmorgangrooming.com",
+	integrations: [
+		preact({
+			compat: true,
+		}),
+		sitemap(),
+		mdx(),
+	],
 
-  vite: {
-    plugins: [tailwindcss()],
-  },
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
