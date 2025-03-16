b47a82d6c00ca6f0efb9be237cd36274d9654af9
Matthew Bratrsovsky <realzing@pm.me>
2025-03-16 16:10:45 -0600

    improve: header styling
    Center the mobile button & implement text wrapping where necessary

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,7 +6,7 @@ export const Nav = ({ route }: { route: string }) => {
 	useScroll();
 
 	return (
-		<div className="grid absolute lg:relative top-4 lg:top-0 right-0 ">
+		<div className="grid absolute lg:relative top-4 lg:top-0 right-0 h-full text-center">
 			<MobileNav />
 			<DesktopNav route={route} />
 		</div>
